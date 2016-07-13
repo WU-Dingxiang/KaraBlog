f894acde5ee40cc61d71a97bcfc3b5bdaa0a37a5
WU-Dingxiang <912966864@qq.com>
2016-07-14 05:02:06 +0800

    1. Directories are sorted.
    2. DES is optimated.

---
+++ karablog/src/main/java/top/karablog/model/User.java
@@ -0,0 +1,34 @@
+﻿package top.karablog.model;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public User setId(int id) {
+		this.id = id;
+		return this;
+	}
+
+	public User setUsername(String username) {
+		this.username = username;
+		return this;
+	}
+
+	public User setPassword(String password) {
+		this.password = password;
+		return this;
+	}
+}
